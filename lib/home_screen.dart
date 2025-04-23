032db189c6076922e0ca2fac63e722dbdea50b7a
waizkamboh <waizkamboh6@gmail.com>
2025-04-23 18:30:55 +0500

    Add initialCameraPosition

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -8,10 +9,17 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+
+  static final CameraPosition _cameraPosition = CameraPosition(
+      target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14,
+  );
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-
+    return  Scaffold(
+        body: GoogleMap(
+            initialCameraPosition: _cameraPosition
+        ),
     );
   }
 }
