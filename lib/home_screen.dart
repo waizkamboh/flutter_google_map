b5184c633b64ab97ea25df63441a3d9e37f971c8
waizkamboh <waizkamboh6@gmail.com>
2025-04-24 16:24:11 +0500

    Add multiple marker on google map

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -14,9 +14,34 @@ class _HomeScreenState extends State<HomeScreen> {
 
   final Completer<GoogleMapController>  _completer = Completer();
   static final CameraPosition _cameraPosition = CameraPosition(
-      target: LatLng(37.42796133580664, -122.085749655962),
+      target: LatLng(30.792640430216533, 73.44316504787581),
     zoom: 14,
   );
+
+  List<Marker> _marker = [];
+  final List<Marker> _list = [
+    Marker(
+        markerId: MarkerId('1'),
+        position: LatLng(30.792640430216533, 73.44316504787581),
+        infoWindow: InfoWindow(
+          title: "My Current Position",
+        )
+    ),
+    Marker(
+        markerId: MarkerId('2'),
+        position: LatLng(30.813890471934677, 73.44562176327965),
+      infoWindow: InfoWindow(
+        title: "My College Position"
+      )
+    )
+  ];
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _marker.addAll(_list);
+  }
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
@@ -25,6 +50,7 @@ class _HomeScreenState extends State<HomeScreen> {
             myLocationEnabled: true,
             mapType: MapType.normal,
             compassEnabled: false,
+            markers: Set<Marker>.of(_marker),
             onMapCreated: (GoogleMapController controller){
               _completer.complete(controller);
             },
