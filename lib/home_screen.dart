45314eb9d4214b3580f150afdc043527a3aa8cb3
waizkamboh <waizkamboh6@gmail.com>
2025-04-24 15:56:33 +0500

    Complete video number 4 the tech brother

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
@@ -10,6 +12,7 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
 
+  final Completer<GoogleMapController>  _completer = Completer();
   static final CameraPosition _cameraPosition = CameraPosition(
       target: LatLng(37.42796133580664, -122.085749655962),
     zoom: 14,
@@ -18,7 +21,13 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return  Scaffold(
         body: GoogleMap(
-            initialCameraPosition: _cameraPosition
+            initialCameraPosition: _cameraPosition,
+            myLocationEnabled: true,
+            mapType: MapType.normal,
+            compassEnabled: false,
+            onMapCreated: (GoogleMapController controller){
+              _completer.complete(controller);
+            },
         ),
     );
   }
