93eddaa3cc3558ecce47925101451fcb5c8041a2
waizkamboh <waizkamboh6@gmail.com>
2025-04-27 15:32:23 +0500

    Convert lat long to address

--- lib/convert_latlong_to_address.dart
+++ lib/convert_latlong_to_address.dart
@@ -22,12 +22,16 @@ class _ConvertLatlongToAddressState extends State<ConvertLatlongToAddress> {
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           Text(latLong),
+          Text(address),
           GestureDetector(
             onTap: () async {
-              List<Location> locations = await locationFromAddress("Gronausestraat 710, Enschede");
-
+              List<Location> locations = await locationFromAddress("QCRV+X46, QCRV+X46, Chak 5/4 L, Okara, Okara, Punjab, , Pakistan");
+              List<Placemark> placemarks = await placemarkFromCoordinates(30.792640430216533, 73.44316504787581);
+               final Placemark place = placemarks.first;
                setState(() {
                  latLong = "${locations.last.latitude} ${locations.last.longitude}";
+                 address = "${place.name}, ${place.street}, ${place.subLocality}, ${place.locality}, ${place.subAdministrativeArea}, ${place.administrativeArea}, ${place.postalCode}, ${place.country}";
+                 print(address);
                });
             },
             child: Padding(
