95989c4067c3cd153da3ac492acf86088628ceaa
waizkamboh <waizkamboh6@gmail.com>
2025-04-28 09:34:08 +0500

    Get User Current Location and move camera to new position

--- lib/user_current_location.dart
+++ lib/user_current_location.dart
@@ -14,14 +14,14 @@ class GetUserCurrentLocation extends StatefulWidget {
   final Completer<GoogleMapController> _controller = Completer();
 class _GetUserCurrentLocationState extends State<GetUserCurrentLocation> {
   static const CameraPosition _cameraPosition = CameraPosition(
-    target: LatLng(30.792640430216533, 73.44316504787581),
+    target: LatLng(30.852222258431745, 73.4497066242562),
     zoom: 14,
   );
 
   final List<Marker> _markers = <Marker>[
     Marker(
         markerId: MarkerId("1"),
-        position: LatLng(30.792640430216533, 73.44316504787581),
+        position: LatLng(30.852222258431745, 73.4497066242562),
         infoWindow: InfoWindow(
           title: "The title of the marker"
         )
@@ -52,9 +52,28 @@ class _GetUserCurrentLocationState extends State<GetUserCurrentLocation> {
       ),
       floatingActionButton: FloatingActionButton(
           onPressed: (){
-            getUserCurrentLocation().then((value){
+            getUserCurrentLocation().then((value) async{
               debugPrint("My current position");
               debugPrint("${value.latitude.toString()} ${value.longitude.toString()}");
+              _markers.add(
+                Marker(
+                  markerId: MarkerId("2"),
+                  position: LatLng(value.latitude, value.longitude),
+                  infoWindow: InfoWindow(
+                    title: "My Current Position",
+                  )
+                )
+              );
+              CameraPosition cameraPosition = CameraPosition(
+                zoom: 14,
+                  target: LatLng(value.latitude, value.longitude)
+              );
+
+              final GoogleMapController controller = await _controller.future;
+              controller.animateCamera(CameraUpdate.newCameraPosition(cameraPosition));
+              setState(() {
+
+              });
             });
           },
           child: Icon(Icons.local_activity),
