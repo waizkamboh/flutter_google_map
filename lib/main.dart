7b52228ed93d6a07d426a342d8ae38aa8ad8925d
waizkamboh <waizkamboh6@gmail.com>
2025-05-13 18:31:37 +0500

    add google map theme json

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter_google_map/custom_marker_info_window.dart';
 import 'package:flutter_google_map/custom_marker_screen.dart';
 import 'package:flutter_google_map/google_places_api.dart';
 import 'package:flutter_google_map/home_screen.dart';
+import 'package:flutter_google_map/network_image_marker.dart';
 import 'package:flutter_google_map/polygone_screen.dart';
 import 'package:flutter_google_map/polyline_screen.dart';
 import 'package:flutter_google_map/style_googlemap_screen.dart';
@@ -22,7 +23,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      home: StyleGooglemapScreen(),
+      home: NetworkImageMarker(),
     );
   }
 }
